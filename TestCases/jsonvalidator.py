4cd722fcb32654dd26747351dd598472e1862efa
vinnuvlsm <vinnuvlsm@gmail.com>
2022-01-02 22:09:07 +0530

    validation function for json output from REST link

---
+++ TestCases/jsonvalidator.py
@@ -0,0 +1,14 @@
+import json
+import re
+
+def json_validator(data):
+    try:
+        json.dumps(data)
+        print("Valid Json")
+        return True
+    except ValueError as error:
+        print("Invalid JSON: %s" % error)
+        return False
+
+
+#regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
